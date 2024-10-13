aba7099ab448e0a07ffd77afaf10aa58516e24a6
Swarnima2004 <swarnima3108@gmail.com>
2024-10-14 01:15:23 +0530

    backend of add item completed

--- app/src/main/java/com/example/adminpanelfoodieworld/addMenu.kt
+++ app/src/main/java/com/example/adminpanelfoodieworld/addMenu.kt
@@ -1,19 +1,35 @@
 package com.example.adminpanelfoodieworld
 
-import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.adminpanelfoodieworld.databinding.ActivityAddMenuBinding
+import com.example.adminpanelfoodieworld.model.AllMenu
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 
 class addMenu : AppCompatActivity() {
-    private val binding : ActivityAddMenuBinding by lazy {
+    //food items
+    private lateinit var foodName: String
+    private lateinit var foodPrice: String
+    private lateinit var foodDescription: String
+    private lateinit var foodIngredient: String
+    private var foodImageUri: Uri? = null
+
+    //firebase
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
+
+    private val binding: ActivityAddMenuBinding by lazy {
         ActivityAddMenuBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,16 +39,82 @@ class addMenu : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+
+        binding.addItemBtn.setOnClickListener {
+            // get data from the admin
+            foodName = binding.foodNameText.text.toString().trim()
+            foodPrice = binding.foodPriceText.text.toString().trim()
+            foodDescription = binding.description.text.toString().trim()
+            foodIngredient = binding.ingredients.text.toString().trim()
+
+            if (!(foodName.isBlank() || foodPrice.isBlank() || foodIngredient.isBlank() || foodIngredient.isBlank())) {
+                uploadData()
+                Toast.makeText(this, "Item added successfully", Toast.LENGTH_SHORT).show()
+                finish()
+            } else {
+                Toast.makeText(this, "PLease fill all the details", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        binding.selectimage.setOnClickListener {
+            pickImage.launch("image/*")
+        }
+
         binding.backButtom.setOnClickListener {
             finish()
         }
-        binding.selectedImage.setOnClickListener{
-            pickImage.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+
+    }
+
+    private fun uploadData() {
+        //getting the reference of the "items" node in the database
+        val menuRef = database.getReference("menu")
+        //generatind unique key for the item
+        val newItemKey = menuRef.push().key
+
+        if (foodImageUri != null) {
+            val storageRef = FirebaseStorage.getInstance().reference
+            val imageRef = storageRef.child("menu_images/${newItemKey}.jpg")
+            val uploadTask = imageRef.putFile(foodImageUri!!)
+
+            uploadTask.addOnSuccessListener {
+                imageRef.downloadUrl.addOnSuccessListener { downloadUrl ->
+
+                    //create menu item
+                    val newItem = AllMenu(
+                        foodName = foodName,
+                        foodPrice = foodPrice,
+                        foodDescription = foodDescription,
+                        foodIngredient = foodIngredient,
+                        foodImage = downloadUrl.toString()
+                    )
+                    newItemKey?.let { key ->
+                        menuRef.child(key).setValue(newItem).addOnSuccessListener {
+                            Toast.makeText(this, "Data added successfully", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                            .addOnFailureListener {
+                                Toast.makeText(this, "Data uploaded failed", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                    }
+                }
+
+            }.addOnFailureListener {
+                Toast.makeText(this, "Image Upload failed", Toast.LENGTH_SHORT).show()
+            }
+
+        } else {
+            Toast.makeText(this, "Please select the image", Toast.LENGTH_SHORT).show()
         }
     }
-    val pickImage = registerForActivityResult(ActivityResultContracts.PickVisualMedia()){ uri ->
-        if(uri != null){
+
+    private val pickImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        if (uri != null) {
             binding.selectedImage.setImageURI(uri)
+            foodImageUri = uri
         }
 
     }
