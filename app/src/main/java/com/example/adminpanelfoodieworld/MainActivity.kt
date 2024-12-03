d6a2ca503dc93d4081bbcb6f370a941cf8467be2
Swarnima2004 <swarnima3108@gmail.com>
2024-12-03 23:49:12 +0530

    entire earning added

--- app/src/main/java/com/example/adminpanelfoodieworld/MainActivity.kt
+++ app/src/main/java/com/example/adminpanelfoodieworld/MainActivity.kt
@@ -7,11 +7,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.adminpanelfoodieworld.databinding.ActivityMainBinding
+import com.example.adminpanelfoodieworld.model.OrderDetails
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
-    private val binding : ActivityMainBinding by lazy {
+    private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private lateinit var database: FirebaseDatabase
+    private lateinit var auth: FirebaseAuth
+    private lateinit var completeOrderReference: DatabaseReference
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,31 +34,92 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         binding.addMenu.setOnClickListener {
-            val intent = Intent(this,addMenu::class.java)
+            val intent = Intent(this, addMenu::class.java)
             startActivity(intent)
         }
         binding.profile.setOnClickListener {
-            val intent = Intent(this,profile::class.java)
+            val intent = Intent(this, profile::class.java)
             startActivity(intent)
         }
 
         binding.addAllMenu.setOnClickListener {
-            val intent = Intent(this,allItemActivity::class.java)
+            val intent = Intent(this, allItemActivity::class.java)
             startActivity(intent)
         }
 
         binding.orderStatus.setOnClickListener {
-            val intent = Intent(this,outForDelivery::class.java)
+            val intent = Intent(this, outForDelivery::class.java)
             startActivity(intent)
         }
 
         binding.CreateUser.setOnClickListener {
-            val intent = Intent(this,newUser::class.java)
+            val intent = Intent(this, newUser::class.java)
             startActivity(intent)
         }
         binding.pendingorder.setOnClickListener {
-            val intent = Intent(this,pendingOrder::class.java)
+            val intent = Intent(this, pendingOrder::class.java)
             startActivity(intent)
         }
+        pendingorders()
+
+        completedOrders()
+
+        entireEarning()
+    }
+
+    private fun entireEarning() {
+        var listOfTotalPay = mutableListOf<Int>()
+        completeOrderReference = FirebaseDatabase.getInstance().reference.child("CompletedOrder")
+        completeOrderReference.addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for( orderSnapShot in snapshot.children){
+                    var completeOrder = orderSnapShot.getValue(OrderDetails::class.java)
+                    completeOrder?.totalPrice?.replace("$","")?.toIntOrNull()
+                        ?.let { i ->
+                            listOfTotalPay.add(i)
+                        }
+                }
+                binding.entireEarning.text = listOfTotalPay.sum().toString()+"$"
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+    }
+
+    private fun completedOrders() {
+        val completedOrderReference = database.reference.child("CompletedOrder")
+        var completedOrders = 0
+        completedOrderReference.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                completedOrders = snapshot.childrenCount.toInt()
+                binding.ordersCompleted.text = completedOrders.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+    }
+
+    private fun pendingorders() {
+        database = FirebaseDatabase.getInstance()
+        val pendingOrderReference = database.reference.child("Order Details")
+        var PendingOrderCount = 0
+        pendingOrderReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                PendingOrderCount = snapshot.childrenCount.toInt()
+                binding.pendingOrder.text = PendingOrderCount.toString()
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
     }
 }
